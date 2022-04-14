8f9d5e579b9ee283545440083ca8a6d6041b56ff
Joan Renfroe <joan.renfroe@gmail.com>
2022-04-13 23:10:20 -0700

    maybe last commit

--- server.js
+++ server.js
@@ -4,59 +4,39 @@ const PORT = 3000;
 const budgets = require("./models/budget.js");
 const morgan = require("morgan");
 
-
 ///////////////// Middleware /////////
 
-app.use( express.urlencoded({extended: false }))
-app.use(express.static('public'));
-app.use(morgan("tiny"))
-
-
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+app.use(morgan("tiny"));
 
 /////////////// Routes ///////////////
 
-
 //Index
-  app.get("/budgets", (req, res) => {
-    res.render("index.ejs", {
-      budgets: budgets,
-    });
+app.get("/budgets", (req, res) => {
+  res.render("index.ejs", {
+    budgets: budgets,
   });
-  
+});
 
+//New
+app.get("/budgets/new", (req, res) => {
+  res.render("new.ejs");
+});
 
-  //New
-  app.get("/budgets/new", (req, res) => {
-    res.render('new.ejs');
+//Create
+app.post("/budgets", (req, res) => {
+  budgets.unshift(req.body);
+  res.redirect("/budgets");
+});
+
+//Show
+app.get("/budgets/:id", (req, res) => {
+  res.render("show.ejs", {
+    entry: budgets[req.params.id],
   });
+});
 
-   //Create
-  app.post("/budgets", (req, res) => {
-    budgets.unshift(req.body)
-    res.redirect("/budgets")
-  });
-
-
-
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-   //Show
-  app.get("/budgets/:id", (req, res) => {
-    res.render('show.ejs', {
-      entry: budgets[req.params.id]
-    });
-  });
-  
-  
-  
-  app.listen(PORT, () => {
-    console.log(`We are listening on port ${PORT}`);
-  });
+app.listen(PORT, () => {
+  console.log(`We are listening on port ${PORT}`);
+});
